c10c0fd811a751944d279c5ac354d9499f9f7049
Ortal Naaman <ortal.n100@gmail.com>
2022-05-21 22:49:14 +0300

    Add project files.

---
+++ Mediator/FacebookGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_HWS2_Proxy_Mediator
+{
+    internal class FacebookGroup : IFacebookGroup
+    {
+        private readonly List<IMember> members = new List<IMember>();
+        public void RegisterUser(IMember member)
+        {
+            members.Add(member);
+        }
+
+        public void SendMessage(string message, IMember member)
+        {
+            members.ForEach(m =>
+            {
+                if (m != member)
+                    m.Receive(message);
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DP_HWS2_Proxy_Mediator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Proxy
+            #endregion
+
+            #region Mediator
+            IFacebookGroup HackerUGroup = new FacebookGroup();
+            IMember ortal = new Member(HackerUGroup, "ortal");
+            IMember avital = new Member(HackerUGroup, "avital");
+            IMember karina = new Member(HackerUGroup, "karina");
+            IMember inna = new Member(HackerUGroup, "inna");
+            IMember moran = new Member(HackerUGroup, "moran");
+
+            HackerUGroup.RegisterUser(ortal);
+            HackerUGroup.RegisterUser(avital);
+            HackerUGroup.RegisterUser(karina);
+            HackerUGroup.RegisterUser(inna);
+            HackerUGroup.RegisterUser(moran);
+
+            ortal.Send("Hello! I'm Ortal");
+            moran.Send("Hello! Wellcome Ortal!");
+
+            #endregion
+        }
+    }
+}
